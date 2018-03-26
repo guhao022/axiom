a32a7c6c8f57a4e5a651bcdccb2c11a787a93af4
Design-05 <378999587@qq.com>
2018-03-26 17:38:38 +0800

    v2.0

--- adapter.go
+++ adapter.go
@@ -3,7 +3,7 @@ package axiom
 // 适配器
 type Adapter interface {
 	GetName() string // 获取连接器名称
-	Process() error  // 处理消息
+	Process(message Message) error  // 处理消息
 	Send() error     // 发送消息
 	Reply() error    // 回复消息
 }
--- robot.go
+++ robot.go
@@ -3,31 +3,82 @@ package axiom
 import (
 	"log"
 	"sync"
+	"fmt"
 )
 
 const DefaultRobotName string = `Axiom`
 
 // 适配器
-type Adapters map[string]Adapter
+var adapters map[string]Adapter
+
+// 注册适配器
+func RegisterAdpater(name string,adp Adapter) {
+	adapters[name] = adp
+}
 
 // robot结构体，包含所有的内部相关数据
 type robot struct {
 	name string // robot 名称，如果没有指定，使用默认名称
-	//adapters    Adapters // 适配器，可注册多个
-	ignoreUsers []string // 用户黑名单
+	ignoreUsers []User // 用户黑名单
 	mu          sync.RWMutex
 	logger      *log.Logger
 }
 
 // 实例化一个robot实例
-func newRobot() *robot {
+func NewRobot() *robot {
 	bot := new(robot)
 
 	bot.name = DefaultRobotName
 
+	bot.ignoreUsers = make([]User, 0)
+
 	return bot
 }
 
-func (bot *robot) SetName(name string) {
+// 获取名称
+func (bot *robot) GetName() string {
+	return bot.name
+}
+
+// 设置名称
+func (bot *robot) SetName(name string) *robot {
 	bot.name = name
+
+	return bot
+}
+
+// 屏蔽用户
+func (bot *robot) IgnoreUsers(user ...User) *robot {
+	if len(user) > 0 {
+		bot.ignoreUsers = append(bot.ignoreUsers, user...)
+	}
+
+	return bot
+}
+
+// 接收消息并传送给时间处理器
+func (bot *robot) Receive(msg *Message) {
+	//
+}
+
+// 运行
+func (bot *robot) Start() error {
+	if len(adapters) <= 0 {
+		return fmt.Errorf("You must add at least one adapter")
+	}
+
+	for name, adp := range adapters {
+		err := adp.Process()
+
+		if err != nil {
+			return fmt.Errorf("The %s adapter process error, error is: %v", name, err)
+		}
+	}
+
+	return nil
+}
+
+// 发送消息
+func (bot *robot) Send() {
+
 }
