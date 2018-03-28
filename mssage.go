fb818879226fd288f95c139f73bb0acd650b2521
Design-05 <378999587@qq.com>
2018-03-28 16:13:45 +0800

    v2.0

--- mssage.go
+++ mssage.go
@@ -1,5 +1,7 @@
 package axiom
 
+import "log"
+
 // 保存机器人发送、接收消息所有的元数据
 type Message struct {
 	Room         string
@@ -9,4 +11,12 @@ type Message struct {
 	ToUserName   string
 	Message      string
 	Direct       bool
+}
+
+func MessageProvider(provider Provider) ListenerFunc {
+	return func(bot *Robot) {
+		log.Printf("bot: changing message provider %T\n", provider)
+		bot.providerIn = provider.IncomingChannel()
+		bot.providerOut = provider.OutgoingChannel()
+	}
 }
