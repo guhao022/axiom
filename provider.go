10c8ce493031f67df5bb6e28938b5335fffe3041
Design-05 <378999587@qq.com>
2018-03-29 15:55:13 +0800

    v2.0

--- provider.go
+++ provider.go
@@ -1,7 +1,73 @@
 package axiom
 
+import (
+	"bufio"
+	"fmt"
+	"bytes"
+	"text/template"
+	"os"
+)
+
 type Provider interface {
 	IncomingChannel() chan Message
 	OutgoingChannel() chan Message
 	Error() error
+}
+
+// 默认实现CLI
+type providerCLI struct {
+	in  chan Message
+	out chan Message
+}
+
+func CLI() *providerCLI {
+	cli := &providerCLI{
+		in:  make(chan Message),
+		out: make(chan Message),
+	}
+	go cli.loop()
+	return cli
+}
+
+func (c *providerCLI) IncomingChannel() chan Message {
+	return c.in
+}
+
+func (c *providerCLI) OutgoingChannel() chan Message {
+	return c.out
+}
+
+func (c *providerCLI) loop() {
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			c.in <- Message{
+				Room:         "CLI",
+				FromUserID:   "CLI",
+				FromUserName: "CLI",
+				Message:      scanner.Text(),
+			}
+		loop:
+			for {
+				select {
+				case msg := <-c.out:
+					os.Stdout.WriteString(processOutMessage(msg))
+				default:
+					break loop
+				}
+			}
+		}
+	}()
+	go func() {
+		for msg := range c.out {
+			os.Stdout.WriteString(processOutMessage(msg))
+		}
+	}()
+}
+
+func processOutMessage(msg Message) string {
+	var finalMsg bytes.Buffer
+	template.Must(template.New("tmpl").Parse(msg.Message)).Execute(&finalMsg, struct{ User string }{msg.ToUserID})
+
+	return fmt.Sprintln("\nout:>", msg.Room, msg.ToUserID, msg.ToUserName, ":", finalMsg.String())
 }
