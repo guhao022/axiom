78d3034d9460c9f5ef6c1bcbd9dcd0e78deeb980
Design-05 <378999587@qq.com>
2018-04-02 17:39:25 +0800

    v2.0

--- adapter.go
+++ adapter.go
@@ -4,27 +4,14 @@ const (
 	DefaultAdapter = `shell`
 )
 
-type Provider interface {
+type Adapter interface {
 	IncomingChannel() chan Message
 	OutgoingChannel() chan Message
 	Name() string
 }
 
-var availableProviders []func(func(string) string) (Provider, bool)
+var availableAdapters []Adapter
 
-func Detect(getenv func(string) string) Provider {
-	for _, ap := range availableProviders {
-		if ret, ok := ap(getenv); ok {
-			if ret.Error() != nil {
-				log.Printf("providers: %T %v", ret, ret.Error())
-				continue
-			}
-			return ret
-		}
-	}
-	log.Println("providers: no message provider found.")
-	log.Println("providers: falling back to CLI.")
-	return CLI()
-}
+
 
 
